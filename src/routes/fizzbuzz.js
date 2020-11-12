ae46aeacfc06b5880f5476154559aca94a4c0a9a
zeako <4206293+zeako@users.noreply.github.com>
2020-11-12 15:48:16 +0200

    validate fizzbuzz schema

--- src/routes/fizzbuzz.js
+++ src/routes/fizzbuzz.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var { body, validationResult } = require('express-validator');
 
 /* POST fizzbuzz. */
 router
     .route('/')
-    .post(function (req, res, next) {
-        res.send('fizzbuzz placeholder');
+    .post([
+        body('count').isInt()
+    ], function (req, res, next) {
+        var errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json({
+                "errors": errors.errors,
+                "response": ""
+            });
+        }
+
+        res.json({
+            "errors": [],
+            "response": "placeholder"
+        })
     })
     .all(function (req, res) {
-        console.log("Hey mami")
         res.status(405).send();
     });
 
